c05ae6db4e36d7fd4c246147404fd0915b9b8dfa
gilles loriquer <gilles.loriquer@gfi.fr>
2020-03-10 12:07:47 +0100

    Initialisation de NoteActivity

--- app/src/main/java/gillesloriquer/com/NoteActivity.java
+++ app/src/main/java/gillesloriquer/com/NoteActivity.java
@@ -2,6 +2,8 @@ package gillesloriquer.com;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,14 +15,36 @@ public class NoteActivity extends AppCompatActivity {
 
     public static final String SELECTED_NOTE = "selected_note";
 
+    // ui components
+    private LinedEditText mLinedEditText;
+    private EditText mEditTitle;
+    private TextView mViewTitle;
+
+    // vars
+    private boolean mIsNewNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-        if (getIntent().hasExtra(SELECTED_NOTE)) {
-            Note note = getIntent().getParcelableExtra(SELECTED_NOTE);
-            Log.d(TAG, "onCreate: " + note.getContent());
+        if (getIncomingIntent()) {
+            // this is a new note, (EDIT MODE)
+        } else {
+            // this is not a new note, (VIEW MODE)
         }
+    }
+
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra(SELECTED_NOTE)) {
+            Note incomingNote = getIntent().getParcelableExtra(SELECTED_NOTE);
+            Log.d(TAG, "getIncomingIntent: " + incomingNote.toString());
+
+            mIsNewNote = false;
+        } else {
+            mIsNewNote = true;
+        }
+
+        return mIsNewNote;
     }
 }
