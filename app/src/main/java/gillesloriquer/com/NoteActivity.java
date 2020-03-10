82e29a4ee0f1a2a782c7e9627832bd30e55fdf4e
gilles loriquer <gilles.loriquer@gfi.fr>
2020-03-10 14:40:11 +0100

    Set initial des propriétés (title et content) d'une Note (NoteActivity)

--- app/src/main/java/gillesloriquer/com/NoteActivity.java
+++ app/src/main/java/gillesloriquer/com/NoteActivity.java
@@ -1,7 +1,6 @@
 package gillesloriquer.com;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +13,7 @@ public class NoteActivity extends AppCompatActivity {
     private static final String TAG = "NoteActivity";
 
     public static final String SELECTED_NOTE = "selected_note";
+    private static final String DEFAULT_NOTE_TITLE = "Note Title";
 
     // ui components
     private LinedEditText mLinedEditText;
@@ -22,29 +22,43 @@ public class NoteActivity extends AppCompatActivity {
 
     // vars
     private boolean mIsNewNote;
+    private Note mInitialNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        mViewTitle = findViewById(R.id.note_text_title);
+        mEditTitle = findViewById(R.id.note_edit_title);
+        mLinedEditText = findViewById(R.id.note_content);
+
         if (getIncomingIntent()) {
-            // this is a new note, (EDIT MODE)
+            setNewNoteProperties();
         } else {
-            // this is not a new note, (VIEW MODE)
+            setNoteProperties();
         }
     }
 
     private boolean getIncomingIntent() {
         if (getIntent().hasExtra(SELECTED_NOTE)) {
-            Note incomingNote = getIntent().getParcelableExtra(SELECTED_NOTE);
-            Log.d(TAG, "getIncomingIntent: " + incomingNote.toString());
-
+            mInitialNote = getIntent().getParcelableExtra(SELECTED_NOTE);
             mIsNewNote = false;
         } else {
             mIsNewNote = true;
         }
 
         return mIsNewNote;
+    }
+
+    private void setNoteProperties() {
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
+    }
+
+    private void setNewNoteProperties() {
+        mViewTitle.setText(DEFAULT_NOTE_TITLE);
+        mEditTitle.setText(DEFAULT_NOTE_TITLE);
     }
 }
