58fa71931a56fa0414ab90b818d143f64a6b69fe
gilles loriquer <gilles.loriquer@gfi.fr>
2020-03-16 11:09:09 +0100

    Implémentation du squelette du repository

---
+++ app/src/main/java/gillesloriquer/com/persistence/NoteRepository.java
@@ -0,0 +1,31 @@
+package gillesloriquer.com.persistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import gillesloriquer.com.models.Note;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note) {
+    }
+
+    public void updateNote(Note note) {
+    }
+
+    public LiveData<List<Note>> retriveNotesTask() {
+        return null;
+    }
+
+    public void deleteNote(Note note) {
+    }
+}
